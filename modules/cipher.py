b387a6c5800c28b3a6f4d2cc5ed11841e2edb229
indicated <81494483+indicatedl@users.noreply.github.com>
2023-05-17 21:12:05 +0300

    Add files via upload

--- modules/cipher.py
+++ modules/cipher.py
@@ -8,7 +8,7 @@ from cryptography.fernet import InvalidToken
 
 class PasswordEncryption:
     '''
-    https://t.me/cryptogovnozavod
+    --- https://t.me/cryptogovnozavod ---
     Encoding and decoding different python objects with password and salt
     :param str password: password
     :param str salt: salt
@@ -22,18 +22,17 @@ class PasswordEncryption:
             iterations=100000
         )
         self.key = base64.urlsafe_b64encode(self.kdf.derive(self.password))
+        self.cipher = Fernet(self.key)
 
     def encrypt(self, data):
         if data is None:
             return None
-        cipher = Fernet(self.key)
-        encrypted_data = cipher.encrypt(pickle.dumps(data))
+        encrypted_data = self.cipher.encrypt(pickle.dumps(data))
         return encrypted_data.decode()
 
     def decrypt(self, encrypted_data):
         try:
-            cipher = Fernet(self.key)
-            decrypted_data = cipher.decrypt(encrypted_data.encode())
+            decrypted_data = self.cipher.decrypt(encrypted_data.encode())
             return pickle.loads(decrypted_data)
         except InvalidToken as e:
             #print('Error: Invalid password!')
